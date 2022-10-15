98616e9f36039a10ca9b63b5240d7fff6b767a5c
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-15 18:15:45 +0600

    blocking and non-blocking code

--- learning-basics/app.js
+++ learning-basics/app.js
@@ -19,15 +19,16 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parseBody = Buffer.concat(body).toString();
       const message = parseBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        // res.writeHead(302, {});
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    // res.writeHead(302, {});
-    return res.end();
   }
 
   console.log(req.url, req.method, req.headers);
