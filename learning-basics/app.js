bafad5117db4da435912b6c627e0ac268d2b9214
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-15 14:44:41 +0600

    routing requests

--- learning-basics/app.js
+++ learning-basics/app.js
@@ -1,6 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title><Enter Message</title></head>");
+    res.write(
+      `<body><form action = "/message" method = "POST"><input type= "text" name="message"><button type="submit">Send Form</button></form></body>`
+    );
+    res.write("</head>");
+    return res.end();
+  } else if (url === "/test") {
+    res.write("<html>");
+    res.write("<head><title>Test Page</title></head>");
+    res.write("<body><h2>Test Page</h2></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
   console.log(req.url, req.method, req.headers);
   //   process.exit();
   res.setHeader("Content-Type", "text/html");
