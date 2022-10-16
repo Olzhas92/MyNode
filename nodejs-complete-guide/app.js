e3c5a269eeb40344643bf1e1970dc9ed45d1d2cc
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-16 20:04:07 +0600

    9. parsing incoming requests

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (request, response, next) => {
   console.log("In another middleware");
-  response.send("<h1>Add Product Page</h1>");
+  response.send(
+    `<form action="/product" method = "POST">
+    <input type="text" name="title"/>
+    <button type="submit">Add Product</button>
+    </form>`
+  );
+});
+
+app.use("/product", (request, response, next) => {
+  console.log(request.body);
+  response.redirect("/");
 });
 
 app.use("/", (request, response, next) => {
-  console.log("In another middleware");
   response.send("<h1>Hello from Express!</h1>");
 });
 
