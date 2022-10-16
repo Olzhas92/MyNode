1b06430605bb4e03d74b5a92b3fb501bf25305a8
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-16 19:17:12 +0600

    7. handling different routes

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use((request, response, next) => {
-  console.log("In the middleware!");
-  next();
+app.use("/add-product", (request, response, next) => {
+  console.log("In another middleware");
+  response.send("<h1>Add Product Page</h1>");
 });
 
-app.use((request, response, next) => {
+app.use("/", (request, response, next) => {
   console.log("In another middleware");
   response.send("<h1>Hello from Express!</h1>");
 });
