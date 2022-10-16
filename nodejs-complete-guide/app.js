aa687453dbdbbc1f9ab93b772f3e43dd2cb7625f
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-16 18:48:36 +0600

    3. adding middleware

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -4,6 +4,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((request, response, next) => {
+  console.log("In the middleware!");
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log("In another middleware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
