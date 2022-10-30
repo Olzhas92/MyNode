948ec2286e98d83bc6f5efb50c68591e0b350453
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-10-30 19:02:05 +0600

    4. Installing and implementing Pug

--- nodejs-complete-guide/routes/admin.js
+++ nodejs-complete-guide/routes/admin.js
@@ -6,15 +6,18 @@ const routeDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (request, response, next) => {
   console.log("In another middleware");
-  console.log(request);
   response.sendFile(path.join(routeDir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (request, response, next) => {
   console.log(request.body);
+  products.push({ title: request.body });
   response.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- nodejs-complete-guide/routes/shop.js
+++ nodejs-complete-guide/routes/shop.js
@@ -4,10 +4,15 @@ const express = require("express");
 
 const routeDir = require("../util/path");
 
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (request, response, next) => {
-  response.sendFile(path.join(routeDir, "views", "shop.html"));
+  // console.log("shop.js", adminData.products);
+  // response.sendFile(path.join(routeDir, "views", "shop.html"));
+
+  response.render("shop");
 });
 
 module.exports = router;
