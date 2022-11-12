56b9cdd6e39589255215929d04f24499892b6fc5
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-11-12 21:36:45 +0600

    1. Preparations

--- nodejs-complete-guide/app.js
+++ nodejs-complete-guide/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -10,7 +12,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -20,4 +21,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(3001, () => {
+  console.log("started...");
+});
